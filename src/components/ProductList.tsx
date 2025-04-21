85ed0dffdbc702a8d1a68e907e0173b566a3b9eb
prajun <prajunbudathoki1@gmail.com>
2025-04-21 09:08:54 +0000

    added the persist middleware to handle the cart on page load

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { fetchAllProducts } from "@/api/FakeApi"
 import { Button } from "./ui/button"
-
+import { useCart } from "@/store"
 
 export default function ProductList() {
     const { data, isLoading, error } = useQuery({
@@ -12,6 +12,11 @@ export default function ProductList() {
     if (isLoading) return <div>Loading...</div>
     if (error) return <div>error while loading products</div>
 
+    const addCart = useCart((state) => state.addCart)
+    const cart = useCart((state) => state.cart)
+    
+    console.log('cart list: ',cart)
+
     return (
         <div className="grid grid-cols-4 gap-4">
             {data?.map((product) => (
@@ -19,10 +24,14 @@ export default function ProductList() {
                     <img src={product.image} alt={product.title} className="w-full h-48 object-cover mb-4" />
                     <h2 className="text-lg font-bold">{product.title}</h2>
                     <p className="text-gray-500">${product.price}</p>
-                    <Button className="mt-2">
+                    <Button className="mt-2" onClick={() => addCart({
+                        ...product,
+                        price: product.price,
+                        quantity: 1
+                    })}>
                         Add to cart
                     </Button>
-                </div>  
+                </div>
             ))}
         </div>
     )
